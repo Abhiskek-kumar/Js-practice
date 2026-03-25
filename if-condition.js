e6cf04bde2f0def01e31213b879c956cb8a5b8d4
Abhiskek-kumar <abhishekkumar97831@gmail.com>
2026-03-25 11:35:15 +0530

    Add some example of if condition

---
+++ if-condition.js
@@ -0,0 +1,10 @@
+//if condition 
+//Conditional statements run different code depending on true or false conditions.
+
+let age = 18 ;
+ if (age >= 18 ){
+    console.log("You can vote ")
+ };
+
+//  👉 If age >= 18 is true → the message will be printed
+//  👉 If it is false → nothing will happen
